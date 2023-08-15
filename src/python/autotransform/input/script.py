60570d460caa197b783cff8401ce27c24d3b37dd
Frank Chen <frxchen@gmail.com>
2023-08-15 16:12:37 -0700

    Improved JSON loading by using json.load instead of json.loads

--- src/python/autotransform/input/script.py
+++ src/python/autotransform/input/script.py
@@ -83,7 +83,7 @@ class ScriptInput(Input):
             proc.check_returncode()
             if uses_result_file:
                 with open(result_file.name, encoding="utf-8") as results:
-                    item_data = json.loads(results.read())
+                    item_data = json.load(results)
             else:
                 item_data = json.loads(proc.stdout.strip())
         return [item_factory.get_instance(item) for item in item_data]
